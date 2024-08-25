9f614b81d60d66db3cbf859a4e8b14ec08ffb740
Nidhi Dilesh <nidhidilesh@gmail.com>
2024-08-25 22:41:56 +0530

    Comments added

--- HackerEarth/Best Index.java
+++ HackerEarth/Best Index.java
@@ -3,11 +3,9 @@ import java.io.InputStreamReader;
 import java.util.*;
 class AddArrays {
     public static void main(String args[] ) throws Exception {
-        //BufferedReader
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());   
         StringTokenizer st = new StringTokenizer(br.readLine());
-        //String[] stringArray = br.readLine().split(" ");
         int[] intArr = new int[N];
         for(int i = 0;i<N;i++){
             intArr[i]=Integer.parseInt(st.nextToken());
@@ -16,14 +14,15 @@ class AddArrays {
         long sum=0;
         for(int j = 0;j<N;j++){
             sum = 0;
+            //k will be updated to index value of an array
             int k = j;
             int loop=1;
+            //if loop is greater than N-k (remaining elements) then stop the loop
             while(N-k>=loop){
                 for(int l = 0;l<loop;l++){
                     sum += intArr[k];
                     k++;
                 }
-                
                 loop++;
             }
             if(sum>max)max=sum;
