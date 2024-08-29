19adc95547baea2a1c99025b604d4afc7f79e8d4
nidhidilesh <nidhidilesh@gmail.com>
2024-08-29 23:11:48 +0530

    Knapsack

---
+++ GeeksForGeeks/Knapsack.java
@@ -0,0 +1,62 @@
+package com.nidhi;
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Item {
+	int value, weight;
+
+	Item(int x, int y) {
+		this.value = x;
+		this.weight = y;
+	}
+}
+
+public class Knapsack {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim());
+		while (t-- > 0) {
+			String inputLine[] = br.readLine().trim().split(" ");
+			int n = Integer.parseInt(inputLine[0]);
+			int w = Integer.parseInt(inputLine[1]);
+			Item[] arr = new Item[n];
+			inputLine = br.readLine().trim().split(" ");
+			for (int i = 0, k = 0; i < n; i++) {
+				arr[i] = new Item(Integer.parseInt(inputLine[k++]), Integer.parseInt(inputLine[k++]));
+			}
+			System.out.println(String.format("%.6f", new Solution().fractionalKnapsack(w, arr, n)));
+		}
+	}
+}
+// } Driver Code Ends
+
+/*
+ * class Item { int value, weight; Item(int x, int y){ this.value = x;
+ * this.weight = y; } }
+ */
+
+class Solution {
+	// Function to get the maximum total value in the knapsack.
+	double fractionalKnapsack(int w, Item arr[], int n) {
+		// Your code here
+		
+		Arrays.sort(arr, new ProfitComparator());
+		double sum = 0d;
+		double temp = 0d;
+		for(int i = 0;i<n;i++) {
+			if(w>=arr[i].weight) {
+				w -= arr[i].weight;
+				sum += arr[i].value;
+			}
+			else {
+				temp = (double)arr[i].value/arr[i].weight;
+				sum += (double)(temp*w);
+				break;
+			}
+		}
+		return sum;
+	}
+}
