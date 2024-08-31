247353357aa3b45e36c4e39b4cb6b599a7c6d13c
nidhidilesh <nidhidilesh@gmail.com>
2024-08-31 18:01:35 +0530

    Subarray with given sum

---
+++ GeeksForGeeks/SubArray.java
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class IntArray {
+    public static int[] input(BufferedReader br, int n) throws IOException {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a) {
+        StringBuilder sb = new StringBuilder();
+        for (int e : a) sb.append(e + " ");
+        System.out.println(sb);
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String str[] = br.readLine().trim().split(" ");
+
+            int n = Integer.parseInt(str[0]);
+            int s = Integer.parseInt(str[1]);
+
+            int[] arr = IntArray.input(br, n);
+
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.subarraySum(arr, n, s);
+
+            IntArray.print(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static ArrayList<Integer> subarraySum(int[] arr, int n, int s) {
+        // code here
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        int sum = 0;
+        int startIndex = 0;
+        int endIndex= 0;
+        for(int i = 0;i<n;i++){
+            sum += arr[i];
+            endIndex = i;
+            while(sum>s && startIndex<endIndex){
+                sum -= arr[startIndex];
+                startIndex++;
+            }
+            if(sum==s && endIndex<=n){
+                list.add(startIndex+1);
+                list.add(endIndex+1);
+                return list;
+            }
+        }
+        list.add(-1);
+        return list;
+    }
+}
