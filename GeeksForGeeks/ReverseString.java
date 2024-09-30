4b71746eead7ec273bf61e3c8b1ae4d26c9ae057
nidhidilesh <nidhidilesh@gmail.com>
2024-09-30 10:25:25 +0530

    Reverse String

---
+++ GeeksForGeeks/ReverseString.java
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Driver {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+            String str = read.readLine();
+            System.out.println(new Reverse().reverseString(str));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class ReverseString {
+    // Complete the function
+    // str: input string
+    public static String reverseString(String s) {
+        // Reverse the string str
+        String result = "";
+        int l = s.length();
+        for(int i = 0;i<l;i++){
+            result = s.charAt(i) + result;
+        }
+        return result;
+    }
+}
